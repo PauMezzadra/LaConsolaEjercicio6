a28bb3c1b4b206770a9e21ba91aaff91133ab0a1
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-26 08:18:27 -0300

    Cambio de código

--- LaConsolaEjercicio6/Program.cs
+++ LaConsolaEjercicio6/Program.cs
@@ -11,38 +11,21 @@ namespace LaConsolaEjercicio6
         static void Main(string[] args)
         {
             ConsoleKeyInfo tecla;
-            // Prevent example from ending if CTL+C is pressed.
-            Console.TreatControlCAsInput = true;
-            bool salidaControl = false;
-            bool salidaShift = false;
-            bool salidaF = false;
 
             do
             {
-                Console.WriteLine("Presiona cualquier tecla. Para salir presiona CTRL + F");
+                Console.WriteLine("Ingrese CTRL + SHIFT + F para salir");
                 tecla = Console.ReadKey();
-                Console.Write(" --- Presionaste las teclas: ");
-                if (tecla.Modifiers == ConsoleModifiers.Alt) Console.Write("ALT + ");
-                if (tecla.Modifiers == ConsoleModifiers.Shift) Console.Write("SHIFT + ");
-                if (tecla.Modifiers == ConsoleModifiers.Control) Console.Write("CTL + ");
-                Console.WriteLine(tecla.Key.ToString());
 
-                if ((tecla.Modifiers & ConsoleModifiers.Control) != 0)
+                if ((tecla.Modifiers & ConsoleModifiers.Control) != 0 
+                        && (tecla.Modifiers & ConsoleModifiers.Shift) != 0
+                            && tecla.Key == ConsoleKey.F
+                                && (tecla.Modifiers & ConsoleModifiers.Alt) == 0)
                 {
-                    salidaControl = true;
-
-                    if ((tecla.Modifiers & ConsoleModifiers.Shift) != 0)
-                    {
-                        salidaShift = true;
-
-                        if (tecla.Key.ToString() == "F")
-                        {
-                            salidaF = true;
-                        }
-                    }
+                    break;
                 }
 
-            } while (!salidaControl || !salidaShift || !salidaF);
+            } while (true);
 
         }
     }
